5664058cf0edee4f05e7e2c239afc28001ee237e
billbo24 <wfloyd1231@gmail.com>
2023-03-04 10:15:23 -0500

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by William Floyd on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
